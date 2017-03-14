2ff6b7b4d2c03f8bf1f6e25e3ed45e8ebe103c56
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-03-14 17:55:38 +0300

    Delegates for request params

---
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/VertxUtil.kt
@@ -0,0 +1,9 @@
+package org.jetbrains.research.kotoed.util
+
+import io.vertx.core.http.HttpServerRequest
+import kotlin.reflect.KProperty
+
+
+operator fun HttpServerRequest.getValue(thisRef: Nothing?, prop: KProperty<*>) =
+        this.getParam(prop.name)
+
