a1fffb3bedc022e4ac28387e295847fe7fd75701
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-03-14 16:02:24 +0300

    Let there be corountines

--- src/main/kotlin/org/jetbrains/research/kotoed/Main.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/Main.kt
@@ -1,27 +1,58 @@
 package org.jetbrains.research.kotoed
 
 import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
+import io.vertx.core.shareddata.AsyncMap
 import io.vertx.ext.web.Router
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.research.kotoed.util.vx
 
 fun main(args: Array<String>) {
-    val vertx = Vertx.vertx()
+    launch(Unconfined) {
+        val vertx = vx<Vertx> { Vertx.clusteredVertx(VertxOptions(), it) }
 
-    vertx.deployVerticle(KotoedServer::class.qualifiedName)
+        vertx.deployVerticle(KotoedServer::class.qualifiedName)
+    }
 }
 
 class KotoedServer : io.vertx.core.AbstractVerticle() {
     override fun start() {
+        launch(Unconfined) {
+            val router = Router.router(vertx)
 
-        val router = Router.router(vertx)
+            val gsms = vx<AsyncMap<String, String>> { vertx.sharedData().getClusterWideMap("gsms", it) }
 
-        router.route("/").handler({ routingContext ->
-            routingContext.response()
-                    .putHeader("content-type", "text/plain")
-                    .end("Kotoed online...")
-        })
+            router.route("/").handler({ ctx ->
+                ctx.response()
+                        .putHeader("content-type", "text/plain")
+                        .end("Kotoed online...")
+            })
 
-        vertx.createHttpServer()
-                .requestHandler({ router.accept(it) })
-                .listen(9000)
+            router.route("/global/create/:key/:value").handler { ctx ->
+                val key = ctx.request().getParam("key")
+                val value = ctx.request().getParam("value")
+                launch(Unconfined) {
+                    vx { gsms.put(key, value, it) }
+                    ctx.response()
+                            .putHeader("content-type", "text/plain")
+                            .end("CREATED: $key -> $value")
+                }
+            }
+
+            router.route("/global/read/:key").handler { ctx ->
+                val key = ctx.request().getParam("key")
+                launch(Unconfined) {
+                    val value = vx<String> { gsms.get(key, it) }
+                    ctx.response()
+                            .putHeader("content-type", "text/plain")
+                            .end("READ: $key -> $value")
+                }
+            }
+
+            vertx.createHttpServer()
+                    .requestHandler({ router.accept(it) })
+                    .listen(9000)
+        }
     }
 }
---
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/Util.kt
@@ -0,0 +1,24 @@
+package org.jetbrains.research.kotoed.util
+
+import io.vertx.core.AsyncResult
+import io.vertx.core.Handler
+import kotlin.coroutines.experimental.suspendCoroutine
+
+@JvmName("vxVoid")
+inline suspend fun vx(crossinline cb: (Handler<AsyncResult<Void?>>) -> Unit) = run {
+    suspendCoroutine<Void?> { cont ->
+        cb(Handler { res ->
+            if (res.succeeded()) cont.resume(res.result())
+            else cont.resumeWithException(res.cause())
+        })
+    }
+}
+
+inline suspend fun <T> vx(crossinline cb: (Handler<AsyncResult<T>>) -> Unit) = run {
+    suspendCoroutine<T> { cont ->
+        cb(Handler { res ->
+            if (res.succeeded()) cont.resume(res.result())
+            else cont.resumeWithException(res.cause())
+        })
+    }
+}
