f312d47011b9e3bc36c49ec7ec665e7e249139c7
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-03-15 15:43:26 +0300

    m

--- src/main/kotlin/org/jetbrains/research/kotoed/util/JsonUtil.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/JsonUtil.kt
@@ -1,7 +1,8 @@
 package org.jetbrains.research.kotoed.util
 
+import io.vertx.core.json.Json
 import io.vertx.core.json.JsonObject
 import kotlin.reflect.KProperty
 
-operator fun JsonObject.getValue(thisRef: Any?, property: KProperty<*>) =
-        this.getValue(property.name)
+inline operator fun<reified T> JsonObject.getValue(thisRef: Any?, property: KProperty<*>): T? =
+        this.getValue(property.name) as T?
