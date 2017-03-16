38ba99bbf83529d1865ed13adc770ecc0b30ebfb
Marat Akhin <1929610+ice-phoenix@users.noreply.github.com>
2017-03-16 12:18:57 +0300

    Working on data layout and TC integration

--- src/main/kotlin/org/jetbrains/research/kotoed/util/JsonUtil.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/JsonUtil.kt
@@ -5,3 +5,10 @@ import kotlin.reflect.KProperty
 
 operator fun JsonObject.getValue(thisRef: Any?, property: KProperty<*>) =
         this.getValue(property.name)
+
+interface Jsonable {
+    fun toJson(): JsonObject
+}
+
+fun JsonObjectOf(vararg pairs: Pair<Any?, Any?>): JsonObject =
+        JsonObject.mapFrom(mapOf(*pairs))
--- src/main/kotlin/org/jetbrains/research/kotoed/util/Util.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/Util.kt
@@ -2,23 +2,42 @@ package org.jetbrains.research.kotoed.util
 
 import io.vertx.core.AsyncResult
 import io.vertx.core.Handler
+import io.vertx.core.eventbus.Message
+import kotlinx.coroutines.experimental.CoroutineExceptionHandler
+import kotlinx.coroutines.experimental.Unconfined
+import kotlin.coroutines.experimental.AbstractCoroutineContextElement
+import kotlin.coroutines.experimental.CoroutineContext
 import kotlin.coroutines.experimental.suspendCoroutine
 
-@JvmName("vxVoid")
-inline suspend fun vx(crossinline cb: (Handler<AsyncResult<Void?>>) -> Unit): Unit = run {
-    suspendCoroutine<Void?> { cont ->
-        cb(Handler { res ->
-            if (res.succeeded()) cont.resume(res.result())
-            else cont.resumeWithException(res.cause())
-        })
-    }
-}
+inline suspend fun vxu(crossinline cb: (Handler<AsyncResult<Void?>>) -> Unit): Void? =
+        suspendCoroutine { cont ->
+            cb(Handler { res ->
+                if (res.succeeded()) cont.resume(res.result())
+                else cont.resumeWithException(res.cause())
+            })
+        }
 
-inline suspend fun <T> vx(crossinline cb: (Handler<AsyncResult<T>>) -> Unit): T = run {
-    suspendCoroutine<T> { cont ->
-        cb(Handler { res ->
-            if (res.succeeded()) cont.resume(res.result())
-            else cont.resumeWithException(res.cause())
-        })
-    }
-}
+inline suspend fun <T> vxt(crossinline cb: (Handler<T>) -> Unit): T =
+        suspendCoroutine { cont ->
+            cb(Handler { res -> cont.resume(res) })
+        }
+
+inline suspend fun <T> vxa(crossinline cb: (Handler<AsyncResult<T>>) -> Unit): T =
+        suspendCoroutine { cont ->
+            cb(Handler { res ->
+                if (res.succeeded()) cont.resume(res.result())
+                else cont.resumeWithException(res.cause())
+            })
+        }
+
+fun <T> UnconfinedWithExceptions(msg: Message<T>) =
+        object : AbstractCoroutineContextElement(CoroutineExceptionHandler.Key), CoroutineExceptionHandler {
+            override fun handleException(context: CoroutineContext, ex: Throwable) {
+                ex.printStackTrace()
+                msg.reply(
+                        JsonObjectOf(
+                                "error" to ex.message
+                        )
+                )
+            }
+        } + Unconfined
