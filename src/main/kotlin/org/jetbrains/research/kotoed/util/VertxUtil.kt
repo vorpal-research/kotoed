98832df06af1a47d8129a4417fa3c0f5a26c6450
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-03-17 21:15:37 +0300

    database support started; logging set up; small fixes here and there

--- src/main/kotlin/org/jetbrains/research/kotoed/util/VertxUtil.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/VertxUtil.kt
@@ -2,10 +2,16 @@ package org.jetbrains.research.kotoed.util
 
 import io.netty.handler.codec.http.HttpHeaderNames
 import io.netty.handler.codec.http.HttpHeaderValues
+import io.vertx.core.Handler
+import io.vertx.core.Vertx
+import io.vertx.core.VertxOptions
 import io.vertx.core.http.HttpServerRequest
 import io.vertx.core.http.HttpServerResponse
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.web.RoutingContext
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import kotlin.reflect.KProperty
 
 operator fun HttpServerRequest.getValue(thisRef: Nothing?, prop: KProperty<*>) =
@@ -16,3 +22,18 @@ fun RoutingContext.jsonResponse() =
                 .putHeader(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
 
 fun HttpServerResponse.end(json: JsonObject) = this.end(json.encode())
+
+suspend fun <T> Vertx.executeBlockingAsync(ordered: Boolean = true, body: () -> T): T  =
+        vxa{
+            this.executeBlocking<T>(
+                    Handler{
+                        fut -> try { fut.complete(body()) } catch (ex: Throwable) { fut.fail(ex) }
+                    },
+                    it)
+        }
+
+suspend fun Vertx.goToEventLoop(): Void =
+        vxt { this.runOnContext(it) }
+
+suspend fun clusteredVertxAsync(opts: VertxOptions = VertxOptions()): Vertx =
+        vxa { Vertx.clusteredVertx(opts, it) }
