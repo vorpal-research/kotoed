eb28a1b2799ffe3289feb74425e01dbc62bdaa4c
Marat Akhin <1929610+ice-phoenix@users.noreply.github.com>
2017-03-20 18:06:41 +0300

    TC build info endpoint

--- src/main/kotlin/org/jetbrains/research/kotoed/teamcity/util/TeamCityApi.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/teamcity/util/TeamCityApi.kt
@@ -1,12 +1,46 @@
 package org.jetbrains.research.kotoed.teamcity.util
 
+data class ApiEndpoint(val path: String) {
+    operator fun invoke(): String = path
+}
+
+interface Locator
+
+object EmptyLocator : Locator {
+    override fun toString(): String = ""
+}
+
+data class StringLocator(val data: String) : Locator {
+    override fun toString(): String = "$data"
+}
+
+data class DimensionLocator(val dimension: String, val value: String) : Locator {
+    override fun toString(): String = "$dimension:$value"
+
+    companion object {
+        fun from(dimension: String, value: String?) =
+                if (value == null) EmptyLocator else DimensionLocator(dimension, value)
+    }
+}
+
+operator infix fun ApiEndpoint.plus(locator: Locator) = ApiEndpoint("$path/$locator")()
+
+operator infix fun Locator.times(locator: Locator) =
+        if (this is EmptyLocator) locator
+        else if (locator is EmptyLocator) this
+        else StringLocator("$this,$locator")
+
+fun Locator.all() =
+        if (this is EmptyLocator) this else StringLocator("?locator=$this")
+
 object TeamCityApi {
     private val endpointRoot = "/app/rest"
 
-    private operator fun String.unaryPlus() = "$endpointRoot/$this"
+    private operator fun String.unaryPlus() = ApiEndpoint("$endpointRoot/$this")
 
     val Projects = +"projects"
     val VcsRoots = +"vcs-roots"
     val BuildTypes = +"buildTypes"
     val BuildQueue = +"buildQueue"
+    val Builds = +"builds"
 }
