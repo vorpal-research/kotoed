3b2653f5c30bcb79ed9221afd01ce49d877755ff
Mikhail Belyaev <2496075+belyaev-mikhail@users.noreply.github.com>
2017-03-21 20:58:35 +0300

    Vcs bindings

--- src/main/kotlin/org/jetbrains/research/kotoed/coroutines/Util.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/coroutines/Util.kt
@@ -1,1 +1,10 @@
 package org.jetbrains.research.kotoed.coroutines
+
+import kotlinx.coroutines.experimental.CoroutineScope
+import kotlinx.coroutines.experimental.Unconfined
+import kotlinx.coroutines.experimental.launch
+import kotlin.coroutines.experimental.CoroutineContext
+
+fun launch(block: suspend CoroutineScope.() -> Unit) {
+    launch(Unconfined, block = block)
+}
---
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/GuavaEx.kt
@@ -0,0 +1,10 @@
+@file:Suppress("NOTHING_TO_INLINE")
+
+package org.jetbrains.research.kotoed.util
+
+import com.google.common.cache.Cache
+
+inline operator fun<K, V> Cache<K, V>.contains(key: K) = this.getIfPresent(key) != null
+inline operator fun<K, V> Cache<K, V>.get(key: K): V? = this.getIfPresent(key)
+inline operator fun<K, V> Cache<K, V>.set(key: K, value: V) = this.put(key, value)
+
