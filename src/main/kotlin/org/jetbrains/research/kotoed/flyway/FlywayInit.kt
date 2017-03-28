f34a3cf72f1953641634c4cb29f13b338bb6b8aa
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-03-28 18:27:27 +0300

    Add Flyway

---
+++ src/main/kotlin/org/jetbrains/research/kotoed/flyway/FlywayInit.kt
@@ -0,0 +1,20 @@
+package org.jetbrains.research.kotoed.flyway
+
+import io.vertx.core.Vertx
+import org.flywaydb.core.Flyway
+import org.jetbrains.research.kotoed.config.Config
+import org.jetbrains.research.kotoed.util.database.getSharedDataSource
+
+object FlywayInit {
+    fun doit(vertx: Vertx) {
+        Flyway().run {
+            dataSource = vertx.getSharedDataSource(
+                    "debug.db",
+                    Config.Debug.DB.Url,
+                    Config.Debug.DB.User,
+                    Config.Debug.DB.Password
+            )
+            migrate()
+        }
+    }
+}
