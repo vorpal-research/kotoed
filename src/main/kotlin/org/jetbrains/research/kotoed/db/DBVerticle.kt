a2f10542a31d4890548a91bfc6be8b9d0fd00a20
Mikhail Belyaev <2496075+belyaev-mikhail@users.noreply.github.com>
2017-04-04 22:25:21 +0300

    Database verticles sketch (only for debug and denizen for now)

---
+++ src/main/kotlin/org/jetbrains/research/kotoed/db/DBVerticle.kt
@@ -0,0 +1,96 @@
+package org.jetbrains.research.kotoed.db
+
+import io.vertx.core.AbstractVerticle
+import io.vertx.core.eventbus.Message
+import io.vertx.core.json.JsonObject
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.research.kotoed.database.Tables
+import org.jetbrains.research.kotoed.database.tables.records.DebugRecord
+import org.jetbrains.research.kotoed.database.tables.records.DenizenRecord
+import org.jetbrains.research.kotoed.util.JsonObject
+import org.jetbrains.research.kotoed.util.UnconfinedWithExceptions
+import org.jetbrains.research.kotoed.util.database.executeKAsync
+import org.jetbrains.research.kotoed.util.database.fetchKAsync
+import org.jetbrains.research.kotoed.util.database.getSharedDataSource
+import org.jetbrains.research.kotoed.util.database.jooq
+import org.jooq.DSLContext
+import org.jooq.Field
+import org.jooq.Table
+import org.jooq.UpdatableRecord
+
+abstract class DatabaseVerticle<R: UpdatableRecord<R>>(
+            val table: Table<R>,
+            val entityName: String = table.name.toLowerCase()
+        ): AbstractVerticle() {
+    val dataSource get() = vertx.getSharedDataSource()
+    val pk get() = table.primaryKey.fields.first() as Field<Any>
+
+    val createAddress = "kotoed.$entityName.create"
+    val updateAddress = "kotoed.$entityName.update"
+    val readAddress   = "kotoed.$entityName.read"
+    val deleteAddress = "kotoed.$entityName.delete"
+
+
+    override fun start() {
+        val eb = vertx.eventBus()
+        eb.consumer<JsonObject>(createAddress){ handleCreate(it) }
+        eb.consumer<JsonObject>(updateAddress){ handleUpdate(it) }
+        eb.consumer<JsonObject>(readAddress){ handleRead(it) }
+        eb.consumer<JsonObject>(deleteAddress){ handleDelete(it) }
+    }
+
+    private suspend fun DSLContext.selectById(id: Any) =
+        select().from(table).where(pk.eq(id)).fetchKAsync().into(JsonObject::class.java).firstOrNull()
+
+    fun handleDelete(message: Message<JsonObject>) = launch(UnconfinedWithExceptions(message)){
+        jooq(dataSource).use {
+            val id = message.body().getValue(pk.name)
+            jooq(dataSource).use {
+                val resp = it
+                        .delete(table)
+                        .where(pk.eq(id))
+                        .returning()
+                        .fetchOne()
+                        ?.into(JsonObject::class.java)
+                message.reply(resp)
+            }
+        }
+    }
+    fun handleRead(message: Message<JsonObject>) = launch(UnconfinedWithExceptions(message)){
+        val id = message.body().getValue(pk.name)
+        jooq(dataSource).use {
+            val resp = it.selectById(id)
+            message.reply(resp)
+        }
+    }
+    fun handleUpdate(message: Message<JsonObject>) = launch(UnconfinedWithExceptions(message)){
+        val id = message.body().getValue(pk.name)
+        jooq(dataSource).use {
+            val resp = it
+                    .update(table)
+                    .set(it.newRecord(table, message.body().map))
+                    .where(pk.eq(id))
+                    .returning()
+                    .fetchOne()
+                    ?.into(JsonObject::class.java)
+
+            message.reply(resp)
+        }
+    }
+    open fun handleCreate(message: Message<JsonObject>) = launch(UnconfinedWithExceptions(message)){
+        jooq(dataSource).use {
+            val resp = it
+                    .insertInto(table)
+                    .set(it.newRecord(table, message.body().map))
+                    .returning()
+                    .fetchOne()
+                    ?.into(JsonObject::class.java)
+
+            message.reply(resp)
+        }
+    }
+
+}
+
+class DebugVerticle: DatabaseVerticle<DebugRecord>(Tables.DEBUG)
+class DenizenVerticle: DatabaseVerticle<DenizenRecord>(Tables.DENIZEN)
--- src/main/kotlin/org/jetbrains/research/kotoed/util/database/DataSourceHolder.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/database/DataSourceHolder.kt
@@ -2,12 +2,16 @@ package org.jetbrains.research.kotoed.util.database
 
 import com.zaxxer.hikari.HikariDataSource
 import io.vertx.core.Vertx
+import org.jetbrains.research.kotoed.config.Config
 import org.jetbrains.research.kotoed.util.getSharedLocal
 import javax.sql.DataSource
 
 data class KotoedDataSource(val ds: DataSource, val url: String) : DataSource by ds
 
-fun Vertx.getSharedDataSource(name: String, url: String, username: String, password: String): KotoedDataSource =
+fun Vertx.getSharedDataSource(name: String = Config.Debug.DB.DataSourceId,
+                              url: String = Config.Debug.DB.Url,
+                              username: String = Config.Debug.DB.User,
+                              password: String = Config.Debug.DB.Password): KotoedDataSource =
         getSharedLocal(name) {
             KotoedDataSource(
                     ds = HikariDataSource().apply {
