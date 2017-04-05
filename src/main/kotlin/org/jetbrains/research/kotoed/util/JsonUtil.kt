c77840af99fe90ac6adeb2ddf1f0884dd46c8dd4
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-04-05 17:36:42 +0300

    Small fixes here and there, itest for /debug/eventbus

--- src/main/kotlin/org/jetbrains/research/kotoed/util/JsonUtil.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/util/JsonUtil.kt
@@ -2,6 +2,7 @@
 
 package org.jetbrains.research.kotoed.util
 
+import io.vertx.core.json.Json
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
 import kotlin.reflect.KClass
@@ -211,5 +212,11 @@ data class JsonDelegate(val obj: JsonObject) {
 }
 
 val JsonObject.delegate get() = JsonDelegate(this)
+
+
+/******************************************************************************/
+
+object JsonEx
+fun JsonEx.decode(enc: String): Any? = JsonArray("""[$enc]""").getValue(0)
 
 /******************************************************************************/
