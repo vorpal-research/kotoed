e5bb8e0aed186b17b60cfa299275a989e6ad88a7
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-07-01 02:41:35 +0300

    Logout

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/routers/Authc.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/routers/Authc.kt
@@ -1,12 +1,17 @@
 package org.jetbrains.research.kotoed.web.routers
 
+import io.netty.handler.codec.http.HttpResponseStatus
 import io.vertx.ext.web.RoutingContext
-import org.jetbrains.research.kotoed.util.HandlerFor
-import org.jetbrains.research.kotoed.util.JsBundle
-import org.jetbrains.research.kotoed.util.Templatize
+import org.jetbrains.research.kotoed.util.*
 
 @HandlerFor("/login")
 @Templatize("login.jade")
 @JsBundle("hello")
 fun login(context: RoutingContext) {}
 
+@HandlerFor("/logout")
+@LoginRequired
+fun logout(context: RoutingContext) {
+    context.session().destroy()
+    context.response().setStatus(HttpResponseStatus.FOUND).putHeader("location", "/").end()
+}
