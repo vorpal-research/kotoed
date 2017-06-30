7b83be248c2e88d98db519aa40f77fe5b229dece
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-06-30 20:34:21 +0300

    Split to modules + some JS helloworlds and build

---
+++ kotoed-js/webpack.config.ts
@@ -0,0 +1,78 @@
+import * as webpack from 'webpack';
+import * as path from 'path';
+
+declare const __dirname;
+
+const src_main = path.resolve(__dirname, "src/main/");
+// Maybe we should put it to webroot/static in kotoed-server's pom.xml
+const dst_path = path.resolve(__dirname, "target/js/webroot/static/");
+
+const config: webpack.Configuration = {
+    context: src_main,
+    entry: {
+        hello: "./ts/hello.ts"
+    },
+    output: {
+        path: dst_path,
+        filename: 'js/[name].bundle.js'
+    },
+    resolve: {
+        extensions: [".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+        alias: {
+            css: path.resolve(src_main, "css"),
+            ts: path.resolve(src_main, "ts"),
+            js: path.resolve(src_main, "js"),
+            less: path.resolve(src_main, "less"),
+
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'less-loader'
+                    },
+                ]
+            },
+
+            {
+                test: /\.(woff2?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file-loader?name=fonts/[name].[ext]"
+            }
+
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({  // TODO this is shit
+            jQuery: 'jquery',
+            $: 'jquery',
+            jquery: 'jquery'
+        })
+    ],
+    devtool: 'inline-source-map',
+};
+
+export default config;
