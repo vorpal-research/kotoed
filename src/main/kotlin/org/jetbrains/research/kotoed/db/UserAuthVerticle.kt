e93e17fe556fed390c22fd7c5cab475380230059
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-07-07 21:48:03 +0300

    Small fixes in oauth stuff

--- src/main/kotlin/org/jetbrains/research/kotoed/db/UserAuthVerticle.kt
+++ src/main/kotlin/org/jetbrains/research/kotoed/db/UserAuthVerticle.kt
@@ -7,11 +7,14 @@ import io.vertx.ext.auth.jdbc.JDBCAuth
 import io.vertx.ext.jdbc.JDBCClient
 import org.jetbrains.research.kotoed.data.db.*
 import org.jetbrains.research.kotoed.database.Tables
+import org.jetbrains.research.kotoed.database.tables.records.DenizenRecord
+import org.jetbrains.research.kotoed.database.tables.records.OauthProfileRecord
 import org.jetbrains.research.kotoed.eventbus.Address
 import org.jetbrains.research.kotoed.util.*
 import org.jetbrains.research.kotoed.util.database.fetchKAsync
 import org.jetbrains.research.kotoed.util.database.getSharedDataSource
 import org.jetbrains.research.kotoed.util.database.jooq
+import org.jooq.Record
 import org.jooq.impl.DSL
 
 @AutoDeployable
@@ -26,56 +29,56 @@ class UserAuthVerticle : AbstractKotoedVerticle(), Loggable {
                     setAuthenticationQuery("SELECT password, salt FROM Denizen WHERE denizenId = ?")
                 }
 
-    @JsonableEventBusConsumerFor(Address.User.Auth.SignUp)
-    suspend fun consumeSignUp(signUpMsg: SignUpMsg): JsonObject {
-        val salt = authProvider.generateSalt()
-        val password = authProvider.computeHash(signUpMsg.password, salt)
+    private fun DenizenRecord.cleanup() = apply {
+        password = null
+        salt = null
+    }
 
-        val dbData = with(Tables.DENIZEN) {
-            mapOf(
-                    DENIZEN_ID.name to signUpMsg.denizenId,
-                    PASSWORD.name to password,
-                    SALT.name to salt
-            )
+    @JsonableEventBusConsumerFor(Address.User.Auth.SignUp)
+    suspend fun consumeSignUp(signUpMsg: SignUpMsg): DenizenRecord {
+        val dbData = DenizenRecord().apply {
+            denizenId = signUpMsg.denizenId
+            salt = authProvider.generateSalt()
+            password = authProvider.computeHash(signUpMsg.password, salt)
         }
 
         return jooq(ds).use {
             with(Tables.DENIZEN) {
                 it.insertInto(this)
-                        .set(it.newRecord(this, dbData))
-                        .returning(ID, DENIZEN_ID)
+                        .set(dbData)
+                        .returning()
                         .fetchOne()
-                        ?.into(JsonObject::class.java)
+                        ?.cleanup()
             }
-        } ?: JsonObject()
+        } ?: throw IllegalStateException("Database error")
     }
 
-    @EventBusConsumerFor(Address.User.Auth.Login)
-    suspend fun consumeLogin(msg: Message<JsonObject>) {
-        fromJson<LoginMsg>(msg.body())
+    @JsonableEventBusConsumerFor(Address.User.Auth.Login)
+    suspend fun consumeLogin(msg: JsonObject): JsonObject {
+        fromJson<LoginMsg>(msg)
 
-        val data = msg.body().rename("denizenId", "username")
+        val data = msg.rename("denizenId", "username")
 
         val user = vxa<User> { authProvider.authenticate(data, it) }
 
-        msg.reply(user.principal().rename("username", "denizenId"))
+        return user.principal().rename("username", "denizenId")
     }
 
     @JsonableEventBusConsumerFor(Address.User.Auth.Info)
-    suspend fun consumeInfo(infoMsg: InfoMsg): JsonObject {
+    suspend fun consumeInfo(infoMsg: InfoMsg): DenizenRecord {
         return jooq(ds).use {
             with(Tables.DENIZEN) {
                 it.selectFrom(this)
                         .where(DENIZEN_ID.eq(infoMsg.denizenId))
                         .fetchKAsync()
-                        .into(JsonObject::class.java)
                         .firstOrNull()
+                        ?.cleanup()
             }
-        } ?: JsonObject()
+        } ?: throw NotFound("User '${infoMsg.denizenId}' not found")
     }
 
     @JsonableEventBusConsumerFor(Address.User.OAuth.SignUp)
-    suspend fun consumeOAuthSignUp(oauthSignUpMsg: OAuthSignUpMsg): JsonObject {
+    suspend fun consumeOAuthSignUp(oauthSignUpMsg: OAuthSignUpMsg): OauthProfileRecord {
         return jooq(ds).use {
             with(Tables.OAUTH_PROFILE) {
                 it.insertInto(
@@ -95,14 +98,13 @@ class UserAuthVerticle : AbstractKotoedVerticle(), Loggable {
                                         .and(Tables.OAUTH_PROVIDER.NAME.eq(oauthSignUpMsg.oauthProvider))
                         )
                         .returning()
-                        .fetchOptional()
-                        .map { it.into(JsonObject::class.java) }
+                        .fetchOne()
             }
-        }.orElse(JsonObject())
+        } ?: throw IllegalStateException("Database error")
     }
 
     @JsonableEventBusConsumerFor(Address.User.OAuth.Login)
-    suspend fun consumeOAuthLogin(oauthLoginMsg: OAuthLoginMsg): JsonObject {
+    suspend fun consumeOAuthLogin(oauthLoginMsg: OAuthLoginMsg): DenizenRecord {
         return jooq(ds).use {
             it.select(*Tables.DENIZEN.fields())
                     .from(Tables.OAUTH_PROFILE)
@@ -113,8 +115,9 @@ class UserAuthVerticle : AbstractKotoedVerticle(), Loggable {
                     .where(Tables.OAUTH_PROVIDER.NAME.eq(oauthLoginMsg.oauthProvider))
                     .and(Tables.OAUTH_PROFILE.OAUTH_USER_ID.eq(oauthLoginMsg.oauthUser))
                     .fetchKAsync()
-                    .into(JsonObject::class.java)
+                    .into(DenizenRecord::class.java)
                     .firstOrNull()
-        } ?: JsonObject()
+                    ?.cleanup()
+        } ?: throw NotFound("User '${oauthLoginMsg.oauthUser}' not found")
     }
 }
