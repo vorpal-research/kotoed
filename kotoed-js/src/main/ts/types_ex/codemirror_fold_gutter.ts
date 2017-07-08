b48ad2c2d154ee1cce8a2f4e85fafee98b34ddac
Kirill Gagarski <kirill.gagarski@gmail.com>
2017-07-08 15:29:35 +0300

    Seems like codemirror types extension was written fine. Removing TODO and doing a little cleanup there

--- kotoed-js/src/main/ts/types_ex/codemirror_fold_gutter.ts
+++ kotoed-js/src/main/ts/types_ex/codemirror_fold_gutter.ts
@@ -1,10 +1,8 @@
-import * as cm from "codemirror"
-
-// TODO This is totally fucked up way of extending existing .d.ts definitions.
-// TODO Rewrite it properly.
 
 declare module "codemirror" {
     export interface EditorConfiguration {
-        foldGutter: boolean
+        foldGutter?: boolean
     }
 }
+
+export = {}
