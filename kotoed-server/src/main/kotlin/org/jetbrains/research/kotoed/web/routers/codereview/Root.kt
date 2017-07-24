aaa18a54a64a7847b52b80cfb527bca037fc168b
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-07-24 17:14:33 +0300

    m

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/routers/codereview/Root.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/routers/codereview/Root.kt
@@ -3,11 +3,12 @@ package org.jetbrains.research.kotoed.web.routers.codereview
 import io.vertx.ext.web.RoutingContext
 import org.jetbrains.research.kotoed.util.HandlerFor
 import org.jetbrains.research.kotoed.util.JsBundle
+import org.jetbrains.research.kotoed.util.LoginRequired
 import org.jetbrains.research.kotoed.util.Templatize
 
 @HandlerFor("/codereview/*")
 @Templatize("code.jade")
-//@LoginRequired
+@LoginRequired
 @JsBundle("code")
 fun handleCode(context: RoutingContext) {
     // Just rendering template. React will do the rest on the client side
