5d1a6abef6cf09b8afb10a7e8a2a3d4711f4259e
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-07-27 21:58:06 +0300

    Resolve #10 + m

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/auth/UavAuthProvider.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/auth/UavAuthProvider.kt
@@ -1,28 +1,56 @@
 package org.jetbrains.research.kotoed.web.auth
 
 import io.vertx.core.AsyncResult
+import io.vertx.core.Future
 import io.vertx.core.Handler
 import io.vertx.core.Vertx
+import io.vertx.core.eventbus.ReplyException
 import io.vertx.core.json.JsonObject
 import io.vertx.ext.auth.AuthProvider
 import io.vertx.ext.auth.User
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.research.kotoed.data.db.InfoMsg
 import org.jetbrains.research.kotoed.eventbus.Address
-import org.jetbrains.research.kotoed.util.rename
+import org.jetbrains.research.kotoed.util.*
 
-class UavAuthProvider(private val vertx: Vertx) : AuthProvider {
-    override fun authenticate(authInfo: JsonObject, handler: Handler<AsyncResult<User>>) {
+class UavAuthProvider(private val vertx: Vertx) : AuthProvider, Loggable {
+
+    private suspend fun authenticateAsync(authInfo: JsonObject, handler: Handler<AsyncResult<User>>) {
         val patchedAI = authInfo.rename("username", "denizenId")
-        vertx.eventBus().send<JsonObject>(Address.User.Auth.Login, patchedAI) {
-            handler.handle(it.map { object : User { // TODO replace with proper user impl
-                override fun isAuthorised(p0: String, p1: Handler<AsyncResult<Boolean>>): User = this
 
-                override fun clearCache(): User = this
+        val userLoginReply: JsonObject
 
-                override fun setAuthProvider(p0: AuthProvider?) {}
-
-                override fun principal(): JsonObject = it.body()
-            } })
+        try {
+            userLoginReply = vertx.eventBus().sendAsync(Address.User.Auth.Login, patchedAI).body()
+        } catch(e: ReplyException) {
+            handler.handle(Future.failedFuture(e.message))
+            return
         }
+        val denizenId: String? = userLoginReply?.get("denizenId") as? String
+
+        if (denizenId == null) {
+            handler.handle(Future.failedFuture("Something went wrong"))
+            throw IllegalStateException("No denizenId in Address.User.Auth.Login reply")
+        }
+
+        val info = vertx
+                .eventBus()
+                .sendAsync(Address.User.Auth.Info, InfoMsg(denizenId = denizenId).toJson())
+                .body()
+
+        val id = info?.get("id") as? Int
+
+        if (id == null) {
+            handler.handle(Future.failedFuture("Something went wrong"))
+            throw IllegalStateException("No id in Address.User.Auth.Info reply")
+        }
+
+        handler.handle(Future.succeededFuture(UavUser(vertx, denizenId, id)))
     }
 
+    override fun authenticate(authInfo: JsonObject, handler: Handler<AsyncResult<User>>) {
+        launch(UnconfinedWithExceptions(this)) {
+            authenticateAsync(authInfo, handler)
+        }
+    }
 }
