e43e6e9217aa2f862f252cb48b4112f4276ecd8e
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-07-27 15:11:21 +0300

    Better auth handling for event bus bridge (without redirect to login form)

---
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/util/routing/RejectAnonymousHandler.kt
@@ -0,0 +1,31 @@
+package org.jetbrains.research.kotoed.util.routing
+
+import io.vertx.ext.auth.AuthProvider
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.AuthHandler
+import io.vertx.ext.web.handler.impl.AuthHandlerImpl
+import org.apache.http.HttpStatus
+
+interface RejectAnonymousHandler : AuthHandler {
+    companion object {
+        fun create(authProvider: AuthProvider): AuthHandler = RejectAnonymousHandlerImpl(authProvider)
+    }
+}
+
+private class RejectAnonymousHandlerImpl(authProvider: AuthProvider) : AuthHandlerImpl(authProvider) {
+    override fun handle(context: RoutingContext) {
+        val session = context.session()
+        if (session != null) {
+            val user = context.user()
+            if (user != null) {
+                // Already logged in, just authorise
+                authorise(user, context)
+            } else {
+                // Now redirect to the login url - we'll get redirected back here after successful login
+                context.response().setStatusCode(HttpStatus.SC_FORBIDDEN).end()
+            }
+        } else {
+            context.fail(NullPointerException("No session - did you forget to include a SessionHandler?"))
+        }
+    }
+}
