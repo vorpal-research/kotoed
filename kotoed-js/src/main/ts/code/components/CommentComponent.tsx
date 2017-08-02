2557e6edbea9cbda6377414bd98c2e928386aba2
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-08-02 13:36:41 +0300

    m for closed comments

--- kotoed-js/src/main/ts/code/components/CommentComponent.tsx
+++ kotoed-js/src/main/ts/code/components/CommentComponent.tsx
@@ -1,16 +1,23 @@
 import * as React from "react";
+import * as ReactMarkdown from "react-markdown";
 import * as moment from "moment";
 
 import {Comment} from "../state";
-import * as ReactMarkdown from "react-markdown";
 
 
 type CommentProps = Comment
 
 export default class CommentComponent extends React.Component<CommentProps, {}> {
+    getPanelClass = () => {
+        if (this.props.state == "open")
+            return "panel-danger";
+        else
+            return "panel-default"
+    };
+
     render() {
         return (
-            <div className="panel panel-danger comment">
+            <div className={`panel ${this.getPanelClass()} comment`}>
                 <div className="panel-heading comment-heading">
                     {`${this.props.authorName}  @ ${moment(this.props.dateTime).format('LLLL')}`}
                 </div>
