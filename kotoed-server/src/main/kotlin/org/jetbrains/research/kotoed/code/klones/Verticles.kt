f4bef3e5c289db434dbf0a6d58f1afa44647d734
Mikhail Belyaev <2496075+belyaev-mikhail@users.noreply.github.com>
2017-08-03 20:53:09 +0300

    Attack of the klones, episode 2

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/code/klones/Verticles.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/code/klones/Verticles.kt
@@ -1,19 +1,25 @@
 package org.jetbrains.research.kotoed.code.klones
 
+import com.intellij.psi.PsiElement
 import com.suhininalex.suffixtree.SuffixTree
+import org.jetbrains.kootstrap.FooBarCompiler
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.psi.KtNamedFunction
+import org.jetbrains.kotlin.psi.KtPsiFactory
+import org.jetbrains.kotlin.psi.psiUtil.collectDescendantsOfType
 import org.jetbrains.research.kotoed.data.api.SubmissionCode
 import org.jetbrains.research.kotoed.data.vcs.CloneStatus
 import org.jetbrains.research.kotoed.database.tables.records.SubmissionRecord
 import org.jetbrains.research.kotoed.eventbus.Address
-import org.jetbrains.research.kotoed.util.AbstractKotoedVerticle
-import org.jetbrains.research.kotoed.util.AutoDeployable
-import org.jetbrains.research.kotoed.util.JsonableEventBusConsumerFor
-import org.jetbrains.research.kotoed.util.sendJsonableAsync
+import org.jetbrains.research.kotoed.util.*
+import ru.spbstu.ktuples.placeholders._0
+import ru.spbstu.ktuples.placeholders.bind
 
 @AutoDeployable
-class KloneVerticle: AbstractKotoedVerticle() {
+class KloneVerticle : AbstractKotoedVerticle(), Loggable {
 
-    val suffixTree = SuffixTree<Int>()
+    val tree = SuffixTree<Token>()
+    val submitted = mutableSetOf<Int>()
 
     @JsonableEventBusConsumerFor(Address.Code.KloneCheck)
     suspend fun handleCheck(sub_: SubmissionRecord) {
@@ -23,9 +29,83 @@ class KloneVerticle: AbstractKotoedVerticle() {
                 SubmissionCode.ListRequest(submission.id)
         )
 
-        if(files.status != CloneStatus.done) return;
+        if (files.status != CloneStatus.done) {
+            log.trace("Repository not cloned yet")
+            return
+        }
+        if (submission.id in submitted) return;
+
+        val compilerEnv = FooBarCompiler.setupMyEnv(CompilerConfiguration())
+
+        val allFiles = files.root?.toFileSeq().orEmpty()
+
+        val ktFiles = allFiles
+                .filter { it.endsWith(".kt") }
+                .toList()
+                .map { filename ->
+                    log.trace("filename = ${filename}")
+                    val resp: SubmissionCode.ReadResponse =
+                            sendJsonableAsync(Address.Api.Submission.Code.Read,
+                                    SubmissionCode.ReadRequest(submissionId = submission.id, path = filename)
+                            )
+                    KtPsiFactory(compilerEnv.project).createFile(filename, resp.contents)
+                }
+
+        val ids = mutableMapOf<Long, PsiElement>()
+
+        ktFiles.asSequence().flatMap { file ->
+            file.collectDescendantsOfType<PsiElement>{ it is KtNamedFunction }.asSequence()
+        }
+                .map { method ->
+                    method to method.dfs { children.asSequence() }
+                            .filter(Token.DefaultFilter)
+                            .map((::makeLiteralToken).bind(_0, submission.id))
+                }
+                .forEach { (method, tokens) ->
+                    val lst = tokens.toList()
+                    log.trace("lst = ${lst}")
+                    val id = tree.addSequence(lst)
+                    ids.put(id, method)
+                }
+
+        submitted += submission.id
+
+        FooBarCompiler.tearDownMyEnv(compilerEnv)
+
+        val clones =
+                tree.root.dfs {
+                    edges
+                            .asSequence()
+                            .map { it.terminal }
+                            .filterNotNull()
+                }.filter { node ->
+                    node.edges
+                            .asSequence()
+                            .all { it.begin == it.end && it.begin == it.sequence.size - 1 }
+                }.filter { node ->
+                    0 == node.parentEdges.last().begin
+                }
+
+        val filtered = clones
+                .map(::CloneClass)
+                .filter { cc -> cc.clones.isNotEmpty() }
+                .toList()
 
 
+        filtered.forEachIndexed { i, cloneClass ->
+            val builder = StringBuilder()
+            val fname = cloneClass.clones
+                    .take(1) // FIXME
+                    .fold("") { _, c -> c.functionName }
+            builder.appendln("($fname) Clone class $i:")
+            cloneClass.clones.forEach { c ->
+                builder.appendln("${c.submissionId}/${c.file.path}:${c.fromLine}:${c.toLine}")
+            }
+            builder.appendln()
+            log.trace(builder)
+        }
+
+        log.trace(clones.joinToString("\n"))
 
     }
 }
