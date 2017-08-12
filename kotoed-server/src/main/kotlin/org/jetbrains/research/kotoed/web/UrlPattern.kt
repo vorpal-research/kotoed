c7d283fea606231ec0bc63003b0a6304ef404e3b
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-08-12 15:48:29 +0300

    UrlPattern object

---
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/UrlPattern.kt
@@ -0,0 +1,36 @@
+package org.jetbrains.research.kotoed.web
+
+object UrlPattern {
+    // TODO maybe make parameter names const values
+    const val Index =  "/"
+
+    object CodeReview {
+        const val Index = "/codereview/:id/*"
+        const val Capabilities = "/codereview-api/caps/:id"
+    }
+
+    object Auth {
+        const val Index = "/login"
+        const val DoLogin = "/login/doLogin"
+        const val DoSignUp = "/login/doSignUp"
+        const val Logout = "/logout"
+    }
+
+    object Submission {
+        const val Results = "/views/submission/:id/results"
+    }
+
+    /**
+     * Each Any in parameter will be converted to string
+     */
+    fun reverse(pattern: String, params: Map<String, Any>, star: Any = ""): String {
+        var url = pattern
+        for ((k, v) in params) {
+            url = url.replace(":$k", "$v")
+        }
+
+        url = url.replace("*", "$star")
+
+        return url
+    }
+}
