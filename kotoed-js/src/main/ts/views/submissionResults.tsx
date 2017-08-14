642bd40b9240bd84a2b7d5195e62d729d07ae77a
Marat Akhin <1929610+ice-phoenix@users.noreply.github.com>
2017-08-14 20:58:41 +0300

    Some stuff done

--- kotoed-js/src/main/ts/views/submissionResults.tsx
+++ kotoed-js/src/main/ts/views/submissionResults.tsx
@@ -1,180 +1,179 @@
 import * as React from "react";
+import {Component} from "react";
 import {render} from "react-dom";
-import Griddle, {GriddleStyleConfig} from "griddle-react";
+import {connect} from "react-redux";
+import Griddle, {
+    ColumnDefinition,
+    GriddleStyleConfig,
+    RowDefinition
+} from "griddle-react";
+
+import {eventBus} from "../eventBus";
+import {Kotoed} from "../util/kotoed-api";
 
 import "less/kotoed-bootstrap/bootstrap.less";
 
-let data = [
-    {
-        "id": 1,
-        "first_name": "Barnett",
-        "last_name": "Drewell",
-        "email": "bdrewell0@umn.edu",
-        "gender": "Male",
-        "ip_address": "59.167.191.219"
-    },
-    {
-        "id": 2,
-        "first_name": "Gillan",
-        "last_name": "Rosling",
-        "email": "grosling1@goo.ne.jp",
-        "gender": "Female",
-        "ip_address": "193.136.86.41"
-    },
-    {
-        "id": 3,
-        "first_name": "Peadar",
-        "last_name": "Eddison",
-        "email": "peddison2@imgur.com",
-        "gender": "Male",
-        "ip_address": "235.40.222.59"
-    },
-    {
-        "id": 4,
-        "first_name": "Cecilla",
-        "last_name": "Radden",
-        "email": "cradden3@creativecommons.org",
-        "gender": "Female",
-        "ip_address": "170.113.253.254"
-    },
-    {
-        "id": 5,
-        "first_name": "Clarice",
-        "last_name": "McGrale",
-        "email": "cmcgrale4@purevolume.com",
-        "gender": "Female",
-        "ip_address": "190.49.178.247"
-    },
-    {
-        "id": 6,
-        "first_name": "Fletcher",
-        "last_name": "Forsbey",
-        "email": "fforsbey5@de.vu",
-        "gender": "Male",
-        "ip_address": "27.24.155.210"
-    },
-    {
-        "id": 7,
-        "first_name": "Barnard",
-        "last_name": "Lunt",
-        "email": "blunt6@wp.com",
-        "gender": "Male",
-        "ip_address": "77.248.1.192"
-    },
-    {
-        "id": 8,
-        "first_name": "Dolf",
-        "last_name": "Winterbottom",
-        "email": "dwinterbottom7@linkedin.com",
-        "gender": "Male",
-        "ip_address": "138.68.249.88"
-    },
-    {
-        "id": 9,
-        "first_name": "Aura",
-        "last_name": "Gorler",
-        "email": "agorler8@spotify.com",
-        "gender": "Female",
-        "ip_address": "150.151.98.210"
-    },
-    {
-        "id": 10,
-        "first_name": "Dimitri",
-        "last_name": "Kelsow",
-        "email": "dkelsow9@thetimes.co.uk",
-        "gender": "Male",
-        "ip_address": "19.222.234.107"
-    },
-    {
-        "id": 11,
-        "first_name": "Kale",
-        "last_name": "Beecheno",
-        "email": "kbeechenoa@trellian.com",
-        "gender": "Male",
-        "ip_address": "150.163.191.21"
-    },
-    {
-        "id": 12,
-        "first_name": "Marena",
-        "last_name": "Jachimiak",
-        "email": "mjachimiakb@marriott.com",
-        "gender": "Female",
-        "ip_address": "118.81.227.196"
-    },
-    {
-        "id": 13,
-        "first_name": "Esta",
-        "last_name": "Parkyns",
-        "email": "eparkynsc@twitpic.com",
-        "gender": "Female",
-        "ip_address": "53.163.122.147"
-    },
-    {
-        "id": 14,
-        "first_name": "Alonzo",
-        "last_name": "Gaisford",
-        "email": "agaisfordd@vinaora.com",
-        "gender": "Male",
-        "ip_address": "230.106.188.188"
-    },
-    {
-        "id": 15,
-        "first_name": "Rad",
-        "last_name": "MacKaig",
-        "email": "rmackaige@nasa.gov",
-        "gender": "Male",
-        "ip_address": "124.255.193.197"
-    },
-    {
-        "id": 16,
-        "first_name": "Victoria",
-        "last_name": "Paget",
-        "email": "vpagetf@epa.gov",
-        "gender": "Female",
-        "ip_address": "19.99.17.225"
-    },
-    {
-        "id": 17,
-        "first_name": "Aloysia",
-        "last_name": "Guillem",
-        "email": "aguillemg@51.la",
-        "gender": "Female",
-        "ip_address": "91.65.5.49"
-    },
-    {
-        "id": 18,
-        "first_name": "Langsdon",
-        "last_name": "Rossin",
-        "email": "lrossinh@howstuffworks.com",
-        "gender": "Male",
-        "ip_address": "32.194.190.44"
-    },
-    {
-        "id": 19,
-        "first_name": "Chico",
-        "last_name": "Championnet",
-        "email": "cchampionneti@ted.com",
-        "gender": "Male",
-        "ip_address": "5.39.193.234"
-    },
-    {
-        "id": 20,
-        "first_name": "Giorgi",
-        "last_name": "Escale",
-        "email": "gescalej@yelp.com",
-        "gender": "Male",
-        "ip_address": "19.43.54.172"
-    }
-];
+enum VerificationStatus {
+    Processed,
+    NotReady,
+    Invalid,
+    Unknown
+}
 
-let awesomeStyleConfig: GriddleStyleConfig = {
-    classNames: {
-        Table: 'table table-bordered table-striped table-hover'
+interface VerificationData {
+    status: VerificationStatus
+    errors: number[]
+}
+
+interface IdRequest {
+    id: number
+}
+
+interface GenericResponse<T> {
+    records: T[]
+    verificationData: VerificationData
+}
+
+interface GenericError {
+    id: number
+    data: any
+}
+
+export interface SubmissionResultTableProps {
+    submissionId: number
+}
+
+export interface SubmissionResultTableState {
+    records: any[]
+    errors: any[]
+}
+
+function sendAsync<Request, Response>(address: string, request: Request): Promise<Response> {
+    return eventBus.awaitOpen().then(_ =>
+        eventBus.send<Request, Response>(address, request)
+    );
+}
+
+export class SubmissionResultTable<ResultT> extends Component<SubmissionResultTableProps, SubmissionResultTableState> {
+
+    constructor(props: SubmissionResultTableProps, context: undefined) {
+        super(props, context);
+
+        this.state = {records: [], errors: []};
+
+        sendAsync<IdRequest, GenericResponse<ResultT>>
+        (Kotoed.Address.Api.Submission.Result.Read, {"id": this.props.submissionId})
+            .then(r => this.processSubmissionResults(r))
+            .catch(ex => this.setState({
+                errors: [{
+                    id: ex.failureCode,
+                    data: ex.message
+                }]
+            }));
+    }
+
+    processSubmissionResults = (json: GenericResponse<ResultT>) => {
+        let records = Promise.resolve(json.records);
+        let errors = 0 < json.verificationData.errors.length
+            ? sendAsync<VerificationData, GenericError[]>
+            (Kotoed.Address.Api.Submission.Error, json.verificationData)
+            : Promise.resolve([] as GenericError[]);
+
+        Promise.all([records, errors])
+            .then(res => {
+                let [r, e] = res;
+                this.setState({records: r, errors: e});
+            });
+    };
+
+    regularStyleConfig: GriddleStyleConfig = {
+        classNames: {
+            Table: 'table table-bordered table-striped table-hover',
+            TableHeading: 'bg-info'
+        }
+    };
+
+    errorStyleConfig: GriddleStyleConfig = {
+        classNames: {
+            Table: 'table table-bordered table-striped table-hover',
+            TableHeading: 'bg-danger'
+        }
+    };
+
+    rowDataSelector = (state: any, {griddleKey}: any) => {
+        return state
+            .get('data')
+            .find((rowMap: any) => rowMap.get('griddleKey') === griddleKey)
+            .toJSON();
+    };
+
+    enhancedWithRowData = connect((state, props) => {
+        return {
+            rowData: this.rowDataSelector(state, props)
+        };
+    });
+
+    succeededTests = ({value, griddleKey, rowData}: { value: any; griddleKey: any; rowData: any }) => {
+        let total = rowData.body.testsuite.tests;
+        let errors = rowData.body.testsuite.errors;
+        let skipped = rowData.body.testsuite.skipped;
+        let failures = rowData.body.testsuite.failures;
+
+        return (
+            <div>{total - (errors + skipped + failures)}</div>
+        );
+    };
+
+    render() {
+        if (0 == this.state.errors.length) {
+            return <Griddle
+                key="records"
+                data={this.state.records}
+                components={{
+                    Filter: () => <span/>,
+                    SettingsToggle: () => <span/>
+                }}
+                styleConfig={this.regularStyleConfig}
+            >
+                <RowDefinition>
+                    <ColumnDefinition id="id"
+                                      title="ID"/>
+                    <ColumnDefinition id="body.testsuite.name"
+                                      title="Type"/>
+                    <ColumnDefinition id="body.testsuite.tests"
+                                      title="Total"/>
+                    <ColumnDefinition id="none"
+                                      title="Success"
+                                      customComponent={this.enhancedWithRowData(this.succeededTests)}/>
+                    <ColumnDefinition id="body.testsuite.errors"
+                                      title="Errors"/>
+                    <ColumnDefinition id="body.testsuite.skipped"
+                                      title="Skipped"/>
+                    <ColumnDefinition id="body.testsuite.failures"
+                                      title="Failures"/>
+                </RowDefinition>
+            </Griddle>;
+        } else {
+            return <Griddle
+                key="errors"
+                data={this.state.errors}
+                components={{
+                    Filter: () => <span/>,
+                    SettingsToggle: () => <span/>
+                }}
+                styleConfig={this.errorStyleConfig}
+            >
+                <RowDefinition>
+                    <ColumnDefinition id="id" title="Error ID"/>
+                    <ColumnDefinition id="data" title="Error data"/>
+                </RowDefinition>
+            </Griddle>;
+        }
     }
 }
 
+
 render(
-    <Griddle data={data}
-             styleConfig={awesomeStyleConfig}/>,
+    <SubmissionResultTable submissionId={39}/>,
     document.getElementById("view-submission-results")
-)
+);
