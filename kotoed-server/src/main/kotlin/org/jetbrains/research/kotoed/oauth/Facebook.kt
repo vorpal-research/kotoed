a383e8f51ce2adb9b956ff03ef7834710f7728bf
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-08-14 21:48:45 +0300

    Facebook OAuth fixed

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/oauth/Facebook.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/oauth/Facebook.kt
@@ -3,6 +3,8 @@ package org.jetbrains.research.kotoed.oauth
 import io.netty.handler.codec.http.HttpHeaderNames
 import io.netty.handler.codec.http.HttpHeaderValues
 import io.vertx.core.Vertx
+import org.jetbrains.research.kotoed.util.asMultiMap
+import org.jetbrains.research.kotoed.util.makeUriQuery
 import org.jetbrains.research.kotoed.util.normalizeUri
 import org.jetbrains.research.kotoed.util.sendAsync
 
@@ -20,12 +22,12 @@ class Facebook(vertx: Vertx) : AbstractOAuthProvider(Name, vertx) {
         val query = mapOf(
                 "fields" to "id",
                 AccessToken to getAccessToken()
-        )
+        ).makeUriQuery()
         val resp = webClient.getAbs("https://graph.facebook.com/v2.10/me$query".normalizeUri())
                 .putHeader("${HttpHeaderNames.ACCEPT}", "${HttpHeaderValues.APPLICATION_JSON}")
                 .putHeader("${HttpHeaderNames.CONTENT_TYPE}", "${HttpHeaderValues.APPLICATION_JSON}")
                 .sendAsync()
-        return resp.bodyAsJsonObject()?.getInteger("id")?.toString() ?: throw OAuthException("Cannot get Facebook id")
+        return resp.bodyAsJsonObject()?.getString("id") ?: throw OAuthException("Cannot get Facebook id")
     }
 
     companion object {
