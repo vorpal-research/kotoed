2be7d02e607d55ad67bf7105d7557da3828e5416
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-08-15 19:22:23 +0300

    Allow Address.Api.Submission.Error

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/eventbus/guardian/Kotoed.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/eventbus/guardian/Kotoed.kt
@@ -22,7 +22,8 @@ fun kotoedPerAddressFilter(vertx: Vertx) = PerAddress(
         Address.Api.Submission.CommentAggregates to SubmissionReady(vertx),
         Address.Api.Submission.Comment.Create to CommentCreateFilter(vertx),
         Address.Api.Submission.Comment.Update to CommentUpdateFilter(vertx),
-        Address.Api.Submission.Result.Read to Permissive
+        Address.Api.Submission.Result.Read to Permissive,
+        Address.Api.Submission.Error to Permissive
 )
 
 val KotoedPerAddressAnonymousFilter = PerAddress(
