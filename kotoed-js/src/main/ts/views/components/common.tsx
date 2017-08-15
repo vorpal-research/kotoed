2d134e636f4a8d7812ba5e4ceb087173a2473406
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-08-15 19:50:08 +0300

    Generalize table view stuff

---
+++ kotoed-js/src/main/ts/views/components/common.tsx
@@ -0,0 +1,33 @@
+import {eventBus} from "../../eventBus";
+
+export interface ErrorDesc {
+    id: number
+    data: any
+}
+
+export enum VerificationStatus {
+    Processed,
+    NotReady,
+    Invalid,
+    Unknown
+}
+
+export interface VerificationData {
+    status: VerificationStatus
+    errors: number[]
+}
+
+export interface IdRequest {
+    id: number
+}
+
+export interface GenericResponse<ResultT> {
+    records: ResultT[]
+    verificationData: VerificationData
+}
+
+export function sendAsync<Request, Response>(address: string, request: Request): Promise<Response> {
+    return eventBus.awaitOpen().then(_ =>
+        eventBus.send<Request, Response>(address, request)
+    );
+}
