9994c7bc16f437e8eef3c978b093f0c494b9bcc2
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-08-15 22:37:52 +0300

    Some navigation stuff

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/navigation/Breadcrumb.kt
+++
@@ -1,7 +0,0 @@
-package org.jetbrains.research.kotoed.web.navigation
-
-data class BreadcrumbElement(val text: String, val active: Boolean, val href: String? = null)
-open class Breadcrumb(val elems: List<BreadcrumbElement>)
-
-operator fun Breadcrumb.plus(elem: BreadcrumbElement) = Breadcrumb(elems + elem)
-operator fun Breadcrumb.plus(otherElems: List<BreadcrumbElement>) = Breadcrumb(elems + otherElems)
