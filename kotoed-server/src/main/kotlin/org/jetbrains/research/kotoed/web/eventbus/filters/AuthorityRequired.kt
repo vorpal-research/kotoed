88cdf521259ece84fa42dc1c12eb85bc409fa78b
Kirill Gagarski <kirill.gagarski@gmail.com>
2017-08-19 01:19:27 +0300

    AuthorityRequired for event bus comment search

---
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/web/eventbus/filters/AuthorityRequired.kt
@@ -0,0 +1,10 @@
+package org.jetbrains.research.kotoed.web.eventbus.filters
+
+import io.vertx.ext.web.handler.sockjs.BridgeEvent
+import org.jetbrains.research.kotoed.util.isAuthorisedAsync
+
+class AuthorityRequired(val authority: String) : BridgeEventFilter {
+    suspend override fun isAllowed(be: BridgeEvent): Boolean =
+            LoginRequired.isAllowed(be) && be.socket().webUser()?.isAuthorisedAsync(authority) ?: false
+
+}
