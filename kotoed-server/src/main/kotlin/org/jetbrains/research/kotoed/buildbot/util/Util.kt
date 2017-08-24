f7132e5a801393164b627cf4d526d9698a601c21
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2017-08-24 16:31:24 +0300

    Support for spaces in buildbot names

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/buildbot/util/Util.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/buildbot/util/Util.kt
@@ -13,12 +13,18 @@ fun <T> HttpRequest<T>.isJson() =
         this.putHeader(HttpHeaderNames.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
 
 object Kotoed2Buildbot {
+    fun string2identifier(str: String): String {
+        val cleaned = str.replace("\\W|^(?=\\d)".toRegex(), "_")
+        val suffix = DigestUtils.sha512Hex(str).take(4)
+        return "${cleaned}_$suffix"
+    }
+
     fun courseName2endpoint(courseName: String) =
             DigestUtils.sha512Hex(courseName).take(32)
 
     fun projectName2builderName(projectName: String) =
-            "buildbot_$projectName"
+            "buildbot_${string2identifier(projectName)}"
 
     fun projectName2schedulerName(projectName: String) =
-            "scheduler_$projectName"
+            "scheduler_${string2identifier(projectName)}"
 }
