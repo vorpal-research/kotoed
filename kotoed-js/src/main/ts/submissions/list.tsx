7a9447bc9367b7ec885e97d13cb817c4d90a15ce
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-08-29 21:26:49 +0300

    Submission details UI draft + m

--- kotoed-js/src/main/ts/submissions/list.tsx
+++ kotoed-js/src/main/ts/submissions/list.tsx
@@ -1,109 +1,16 @@
 import * as React from "react";
-import {Table,
-    Glyphicon,
-    Tooltip,
-    OverlayTrigger} from "react-bootstrap";
+
 import {Kotoed} from "../util/kotoed-api";
 import {render} from "react-dom";
-import * as Spinner from "react-spinkit"
-import * as moment from "moment";
-
 import {fetchPermissions} from "./remote";
 import {
     SearchTableWithVerificationData
 } from "../views/components/searchWithVerificationData";
-import {WithVerificationData} from "../data/verification";
 import snafuDialog from "../util/snafuDialog";
-import {JumboProject, SubmissionToRead} from "../data/submission";
-
 import "less/projects.less"
-import {makeSubmissionResultsUrl, makeSubmissionReviewUrl} from "../util/url";
+import {renderSubmissionTable} from "./table";
+import {SubmissionComponent, SubmissionWithVer} from "./SubmissionComponent";
 import {SubmissionCreate} from "./create";
-
-type SubmissionWithVer = SubmissionToRead & WithVerificationData
-
-class SubmissionComponent extends React.PureComponent<SubmissionWithVer> {
-    constructor(props: SubmissionWithVer) {
-        super(props);
-    }
-
-    private linkToSubDetails = (text: string): JSX.Element => {
-        if (this.props.verificationData.status === "Processed")
-            return <a href={Kotoed.UrlPattern.NotImplemented}>{text}</a>;
-        else
-            return <span className={"grayed-out"}>{text}</span>
-    };
-
-
-    private linkToResults = (): JSX.Element => {
-        if (this.props.verificationData.status === "Processed")
-            return <a href={makeSubmissionResultsUrl(this.props.id)}>Results</a>;
-        else
-            return <span className={"grayed-out"}>Results</span>
-    };
-
-
-    private linkToReview = (): JSX.Element => {
-        if (this.props.verificationData.status === "Processed")
-            return <a href={makeSubmissionReviewUrl(this.props.id)}>Review</a>;
-        else
-            return <span className={"grayed-out"}>Review</span>
-    };
-
-
-    private readonly invalidTooltip = <Tooltip id="tooltip">This submission is invalid</Tooltip>;
-    private readonly closedTooltip = <Tooltip id="tooltip">This submission is closed</Tooltip>;
-
-    private readonly spinner =  <Spinner name="three-bounce" color="gray" fadeIn="none" className="display-inline"/>;
-    private readonly exclamation = <OverlayTrigger placement="right" overlay={this.invalidTooltip}>
-        <span className="text-danger">
-            <Glyphicon glyph="exclamation-sign"/>
-        </span>
-    </OverlayTrigger>;
-    private readonly lock = <OverlayTrigger placement="right" overlay={this.closedTooltip}>
-        <span className="text-danger">
-            <Glyphicon glyph="lock"/>
-        </span>
-    </OverlayTrigger>;
-
-
-    private renderIcon = (): JSX.Element | null => {
-        let {status} = this.props.verificationData;
-        let {state} = this.props;
-        switch (status) {
-            case "NotReady":
-            case "Unknown":
-                return this.spinner;
-            case "Invalid":
-                return this.exclamation;
-            case "Processed":
-                switch (state) {
-                    case "pending":
-                        return this.spinner;
-                    case "invalid":
-                        return this.exclamation;
-                    case "open":
-                        return null;
-                    case "obsolete":
-                        return null; // Should not happen here
-                    case "closed":
-                        return this.lock;
-                    default:
-                        return null;
-                }
-        }
-    };
-    render() {
-        return <tr>
-            <td>{this.linkToSubDetails(this.props.id.toString())}{" "}{this.renderIcon()}</td>
-            <td>{this.linkToSubDetails(moment(this.props.datetime).format('LLLL'))}</td>
-            <td>{this.props.revision}</td>
-            <td>{this.linkToResults()}</td>
-            <td>{this.linkToReview()}</td>
-        </tr>
-    }
-
-}
 
 class SubmissionList extends React.Component<{}, {canCreateSubmission: boolean}> {
 
@@ -123,28 +30,9 @@ class SubmissionList extends React.Component<{}, {canCreateSubmission: boolean}>
 
     toolbarComponent = (redoSearch: () => void) => {
         if (this.state.canCreateSubmission)
-            return <SubmissionCreate onCreate={redoSearch} projectId={id_}/>
+            return <SubmissionCreate onCreate={redoSearch} projectId={id_}/>;
         else
             return null;
-    };
-
-
-    renderTable = (children: Array<JSX.Element>): JSX.Element => {
-        return (
-            <Table striped bordered condensed hover responsive>
-                <thead>
-                <tr>
-                    <th>Id</th>
-                    <th>Created at</th>
-                    <th>Revision</th>
-                    <th>Results</th>
-                    <th>Review</th>
-                </tr>
-                </thead>
-                <tbody>
-                {children}
-                </tbody>
-            </Table>)
     };
 
     render() {
@@ -161,7 +49,7 @@ class SubmissionList extends React.Component<{}, {canCreateSubmission: boolean}>
                         }
                     }
                 }}
-                wrapResults={this.renderTable}
+                wrapResults={renderSubmissionTable}
                 elementComponent={(key, c: SubmissionWithVer) => <SubmissionComponent {...c} key={key} />}
                 toolbarComponent={this.toolbarComponent}
             />
