3e25c7c24b999aaf004a4860b15f946009c318a2
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-09-05 15:22:02 +0300

    warning when creating a new sub instead of resub

--- kotoed-js/src/main/ts/submissions/create.tsx
+++ kotoed-js/src/main/ts/submissions/create.tsx
@@ -5,6 +5,7 @@ import {eventBus, SoftError} from "../eventBus";
 import {ChangeEvent, KeyboardEvent} from "react";
 import {CreateRequest, SubmissionToRead} from "../data/submission";
 import {DbRecordWrapper} from "../data/verification";
+import {sendAsync} from "../views/components/common";
 
 interface SubmissionCreateProps {
     onCreate: (newId: number) => void
@@ -14,7 +15,9 @@ interface SubmissionCreateProps {
 
 
 interface SubmissionCreateState {
-    showModal: boolean
+    showRevisionModal: boolean
+    showAreYouSureModal: boolean,
+    suggestedParent: number | null,
     remoteError?: string
     revision: string
 }
@@ -24,8 +27,10 @@ export class SubmissionCreate extends React.Component<SubmissionCreateProps, Sub
     constructor(props: SubmissionCreateProps) {
         super(props);
         this.state = {
-            showModal: false,
-            revision: ""
+            showRevisionModal: false,
+            showAreYouSureModal: false,
+            revision: "",
+            suggestedParent: null
         }
     }
 
@@ -39,15 +44,28 @@ export class SubmissionCreate extends React.Component<SubmissionCreateProps, Sub
     };
 
 
-    showModal = () => {
-        this.setState({showModal: true})
+    showRevisionModal = () => {
+        this.setState({showRevisionModal: true})
     };
 
-    hideModal = () => {
+    hideRevisionModal = () => {
         this.dismissRemoteError();
         this.setState({
-            showModal: false,
-            revision: ""
+            showRevisionModal: false
+        });
+    };
+
+    showAreYouSureModal = (suggestedParent: number | null) => {
+        this.setState({
+            showAreYouSureModal: true,
+            suggestedParent
+        })
+    };
+
+    hideAreYouSureModal = () => {
+        this.setState({
+            showAreYouSureModal: false,
+            suggestedParent: null
         });
     };
 
@@ -55,7 +73,41 @@ export class SubmissionCreate extends React.Component<SubmissionCreateProps, Sub
         this.setState({remoteError: undefined});
     };
 
-    tryCreate = async (revision: string | null = null) => {
+    dismissState = () => {
+        this.setState({
+            revision: "",
+            suggestedParent: null
+        })
+    };
+
+
+    checkExistingOrTryCreate = async (revision: string | null = null) => {
+        if (this.props.parentSubmission)
+            await this.tryCreate(revision);
+
+        let existing = await sendAsync<any, Array<SubmissionToRead>>(Kotoed.Address.Api.Submission.List, {
+            pageSize: 2,
+            currentPage: 0,
+            text: "",
+            find: {
+                projectId: this.props.projectId,
+                stateIn: [
+                    "open",
+                    "pending"
+                ]
+            }
+        });
+
+        if (existing.length == 0)
+            await this.tryCreate();
+        else {
+            let suggestion = existing.length == 1 ? existing[0].id : null;
+            this.hideRevisionModal();
+            this.showAreYouSureModal(suggestion);
+        }
+    };
+
+    tryCreate = async (revision: string | null = null, parentSubmission: number | null = null) => {
         if (revision !== null && revision.trim() === "")
             revision = null;
 
@@ -63,13 +115,18 @@ export class SubmissionCreate extends React.Component<SubmissionCreateProps, Sub
             let newSub = await eventBus.send<CreateRequest, DbRecordWrapper<SubmissionToRead>>(Kotoed.Address.Api.Submission.Create, {
                 revision: revision,
                 projectId: this.props.projectId,
-                parentSubmissionId: this.props.parentSubmission || null
+                parentSubmissionId: parentSubmission || this.props.parentSubmission || null
             });
+            this.hideRevisionModal();
+            this.hideAreYouSureModal();
+            this.dismissState();
             this.props.onCreate(newSub.record.id);
-            this.hideModal();
         } catch (error) {
-            if (!(error instanceof SoftError))
-                this.hideModal();
+            this.dismissState();
+            if (!(error instanceof SoftError)) {
+                this.hideRevisionModal();
+                this.hideAreYouSureModal();
+            }
             else {
                 this.setState({
                     remoteError: error.message
@@ -81,16 +138,19 @@ export class SubmissionCreate extends React.Component<SubmissionCreateProps, Sub
 
 
 
-    handleEnter = (event: KeyboardEvent<FormControl>) => event.key === "Enter" && this.tryCreate(this.state.revision);
+    handleEnter = (event: KeyboardEvent<FormControl>) => event.key === "Enter" && this.checkExistingOrTryCreate(this.state.revision);
 
     render() {
         return <SplitButton
-                bsSize="lg"
-                bsStyle="success"
-                title={this.props.parentSubmission === undefined ? "Submit" : "Resubmit"}
-                onClick={() => this.tryCreate()} id="submit-dropdown">
-                <MenuItem eventKey="specify-revision" onClick={this.showModal}>Specify revision</MenuItem>
-                <Modal show={this.state.showModal} onHide={this.hideModal}>
+                    bsSize="lg"
+                    bsStyle="success"
+                    title={this.props.parentSubmission === undefined ? "Submit" : "Resubmit"}
+                    onClick={() => this.checkExistingOrTryCreate()} id="submit-dropdown">
+            <MenuItem eventKey="specify-revision" onClick={this.showRevisionModal}>Specify revision</MenuItem>
+            <Modal show={this.state.showRevisionModal} onHide={() => {
+                this.hideRevisionModal();
+                this.dismissState();
+            }}>
                     <Modal.Header closeButton>
                         <Modal.Title>Create new submission</Modal.Title>
                     </Modal.Header>
@@ -114,9 +174,39 @@ export class SubmissionCreate extends React.Component<SubmissionCreateProps, Sub
                         </Form>
                     </Modal.Body>
                     <Modal.Footer>
-                        <Button bsStyle="success" onClick={() => this.tryCreate(this.state.revision)}>Create</Button>
+                        <Button bsStyle="success" onClick={() => this.checkExistingOrTryCreate(this.state.revision)}>Create</Button>
                     </Modal.Footer>
                 </Modal>
+            <Modal show={this.state.showAreYouSureModal} onHide={() => {
+                this.hideAreYouSureModal();
+                this.dismissState();
+            }}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Are you sure?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p>
+                        It seems like you already have an open submission. You probably should resubmit it instead
+                        of creating a new one.
+                    </p>
+                </Modal.Body>
+                <Modal.Footer>
+                    {this.state.suggestedParent &&
+                    <Button bsStyle="success" onClick={() => this.tryCreate(this.state.revision, this.state.suggestedParent)}>
+                        {`Resubmit to #${this.state.suggestedParent}`}
+                    </Button>}
+                    <Button bsStyle="danger" onClick={() => this.tryCreate(this.state.revision)}>
+                        Submit anyway
+                    </Button>
+                    <Button onClick={() => {
+                        this.hideAreYouSureModal();
+                        this.dismissState();
+                    }}>
+                        Cancel
+                    </Button>
+
+                </Modal.Footer>
+            </Modal>
             </SplitButton>;
     }
 }
