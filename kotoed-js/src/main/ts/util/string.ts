a494038f6459d6f02c74bd17cd146b430d95e493
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2017-09-19 14:22:51 +0300

    truncateString bugfix

--- kotoed-js/src/main/ts/util/string.ts
+++ kotoed-js/src/main/ts/util/string.ts
@@ -2,6 +2,7 @@ export function truncateString(str: string, len: number): string {
     if (str.length <= len) return str;
     else {
         let truc = (len - 3) / 2;
-        return str.substr(0, truc) + "..." + str.substr(str.length - truc, truc)
+        let [left, right] = [Math.ceil(truc), Math.floor(truc)];
+        return str.substr(0, left) + "..." + str.substr(str.length - right, right)
     }
 }
