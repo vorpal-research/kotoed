0ae46b68e9e3e061324f516149930cdf70d505a1
Mikhail Belyaev <2496075+belyaev-mikhail@users.noreply.github.com>
2017-09-27 22:36:46 +0300

    Code annotations in codemirror, removed test data

--- kotoed-js/src/main/ts/code/remote/annotations.ts
+++ kotoed-js/src/main/ts/code/remote/annotations.ts
@@ -2,21 +2,5 @@ import {ReviewAnnotations} from "../state/annotations";
 import {Map} from "immutable";
 
 export async function fetchAnnotations(submissionId: number): Promise<ReviewAnnotations> {
-    return Map({
-        "test/lesson2/task1/Tests.kt": [{
-            message: "Да у тебя тут бага, чувак!",
-            severity: "warning" as "warning",
-            position: {
-                line: 12,
-                col: 12
-            }
-        },{
-            message: "Да у тебя тут ваще бага, чувак!",
-            severity: "error" as "error",
-            position: {
-                line: 12,
-                col: 10
-            }
-        }]
-    })
+    return Map()
 }
