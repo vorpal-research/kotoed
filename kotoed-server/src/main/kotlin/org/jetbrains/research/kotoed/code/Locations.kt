5c3132eb0ba93180bdda4229d4aba66815b319f7
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-10-02 21:32:31 +0300

    Trying to fix missing comments

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/code/Locations.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/code/Locations.kt
@@ -1,8 +1,11 @@
 package org.jetbrains.research.kotoed.code
 
+import org.jetbrains.research.kotoed.util.GlobalLogging.log
 import org.jetbrains.research.kotoed.util.Jsonable
 import org.wickedsource.diffparser.api.model.Diff
+import org.wickedsource.diffparser.api.model.Hunk
 import org.wickedsource.diffparser.api.model.Line
+import org.wickedsource.diffparser.api.model.Range
 
 enum class FileLocType { NORMAL, NON_EXISTENT, UNKNOWN }
 
@@ -21,6 +24,9 @@ private fun String.asFilename() =
             else -> Filename(FileLocType.NORMAL, this)
         }
 
+private val Range.lineEnd get() = lineStart + lineCount
+private val Hunk.lineShift get() = toFileRange.lineEnd - fromFileRange.lineEnd
+
 fun Location.applyDiffs(diffs: List<Diff>): Location {
     val diff = diffs.find { it.fromFileName == filename.path }
     diff ?: return this
@@ -28,21 +34,42 @@ fun Location.applyDiffs(diffs: List<Diff>): Location {
         val fromLoc = it.fromFileRange
         line in (fromLoc.lineStart .. (fromLoc.lineStart + fromLoc.lineCount))
     }
-    hunk ?: return this
+
+    if(hunk == null) {
+        val preHunk = diff.hunks.firstOrNull {
+            val fromLoc = it.fromFileRange
+            line > fromLoc.lineStart
+        }
+
+        preHunk ?: return this
+
+        log.info("preHunk.lineShift = ${preHunk.lineShift}")
+        log.info("line = ${line}")
+
+        return Location(diff.toFileName.asFilename(), line + preHunk.lineShift, col)
+    }
 
     var curFrom = hunk.fromFileRange.lineStart
     var curTo = hunk.toFileRange.lineStart
 
+    var lastNeutralFrom = curFrom;
+    var lastNeutralTo = curTo;
+
     for(line in hunk.lines) {
         when(line.lineType) {
             null -> throw IllegalArgumentException("Hunk line should not be null")
-            Line.LineType.NEUTRAL -> { ++curFrom; ++curTo }
+            Line.LineType.NEUTRAL -> {
+                lastNeutralFrom = curFrom
+                lastNeutralTo = curTo
+                ++curFrom; ++curTo
+            }
             Line.LineType.TO -> { ++curTo }
             Line.LineType.FROM -> { ++curFrom }
         }
 
         if(curFrom == this.line) break
     }
+    curTo = (curFrom - lastNeutralFrom) + lastNeutralTo
 
     return Location(diff.toFileName.asFilename(), curTo, col)
 
