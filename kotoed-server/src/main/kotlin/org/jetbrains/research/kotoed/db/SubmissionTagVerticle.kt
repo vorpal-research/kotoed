127811745956cec591bcdce010ad94e9e719e23c
Mikhail Belyaev <Mikhail Belyaev@localhost>
2017-10-13 19:34:11 +0300

    Stop tag verticle from whining on create/delete

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/db/SubmissionTagVerticle.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/db/SubmissionTagVerticle.kt
@@ -8,10 +8,48 @@ import org.jetbrains.research.kotoed.eventbus.Address
 import org.jetbrains.research.kotoed.util.AutoDeployable
 import org.jetbrains.research.kotoed.util.JsonObject
 import org.jetbrains.research.kotoed.util.JsonableEventBusConsumerFor
+import org.jetbrains.research.kotoed.util.NotFound
 import org.jetbrains.research.kotoed.util.database.executeKAsync
+import org.jetbrains.research.kotoed.util.database.into
 
 @AutoDeployable
 class SubmissionTagVerticle : CrudDatabaseVerticleWithReferences<SubmissionTagRecord>(Tables.SUBMISSION_TAG) {
+
+    override suspend fun handleCreate(message: SubmissionTagRecord): SubmissionTagRecord {
+        log.trace("Create requested in table ${table.name}:\n$message")
+
+        for (field in table.primaryKey.fieldsArray) {
+            message.reset(field)
+        }
+
+        return db {
+            sqlStateAware {
+                insertInto(table)
+                        .set(message)
+                        .onDuplicateKeyIgnore()
+                        .returning()
+                        .fetch()
+                        .into(recordClass)
+                        .first()
+            }
+        }
+    }
+
+    suspend override fun handleDelete(message: SubmissionTagRecord): SubmissionTagRecord {
+        val id = message.getValue(pk.name)
+        log.trace("Delete requested for id = $id in table ${table.name}")
+
+        return db {
+            delete(table)
+                    .where(pk.eq(id))
+                    .returning()
+                    .fetch()
+                    .into(recordClass)
+                    .firstOrNull()
+                    ?: SubmissionTagRecord().apply{ this.id = id.toString().toInt() }
+        }
+    }
+
     @JsonableEventBusConsumerFor(Address.DB.Submission.Tags.Update)
     suspend fun consumeSubmissionTagsUpdate(query: Submission.TagUpdateQuery): JsonObject {
         return dbWithTransactionAsync {
