12d5bf12106a43f35bdf0f1f1456fa567d7fd111
Mikhail Belyaev <2496075+belyaev-mikhail@users.noreply.github.com>
2017-12-08 20:13:57 +0300

    Trying to fix mailing issues

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/notification/MailVerticle.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/notification/MailVerticle.kt
@@ -65,7 +65,7 @@ class MailVerticle : AbstractNotificationVerticle(), Loggable {
 
         log.info("Sending email to $denizen")
 
-        spawn(Pool) { try{ mailer.sendMail(email) } catch(ex: Exception) { log.error("", ex) } }
+        spawn(Pool) { try{ mailer.sendMail(email, true) } catch(ex: Exception) { log.error("", ex) } }
     }
 
 }
