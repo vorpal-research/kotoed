2c68fa30703dacc986ad93113446ad2e87b48b53
Mikhail Belyaev <Mikhail Belyaev@localhost>
2018-08-07 18:52:04 +0300

    Removed all no-more-needed verticles + kinda fixed session issues

--- kotoed-server/src/main/resources/db/migration/V65__Add_environment_to_build_template.sql
+++ kotoed-server/src/main/resources/db/migration/V65__Add_environment_to_build_template.sql
@@ -5,5 +5,5 @@ ALTER TABLE build_template ADD COLUMN IF NOT EXISTS
 UPDATE build_template SET environment = '{
   "MAVEN_OPTS" : "-Xmx512M",
   "GIT_SSH_COMMAND" : "ssh -i id_rsa.kotoed",
-  "GIT_ASKPASS" : "echo ''"
+  "GIT_ASKPASS" : "true"
 }'::jsonb WHERE id =  1
