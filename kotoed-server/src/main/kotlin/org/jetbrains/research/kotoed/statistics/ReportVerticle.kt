f5e81846a9559bbfe687edb6eb563ceb2539d330
Marat Akhin <akhin@kspt.icc.spbstu.ru>
2018-08-23 15:26:37 +0300

    ReportVerticle switched to new scoring system

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/statistics/ReportVerticle.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/statistics/ReportVerticle.kt
@@ -10,73 +10,79 @@ import org.jetbrains.research.kotoed.util.*
 import org.jetbrains.research.kotoed.util.database.toRecord
 import java.time.OffsetDateTime
 
-data class ReportRequest(val date: OffsetDateTime?): Jsonable
-data class ReportReply(val open: List<Pair<String, Int>>, val closed: List<Pair<String, Int>>): Jsonable
+data class ReportRequest(val date: OffsetDateTime?) : Jsonable
+data class ReportReply(val open: List<Pair<String, Double>>, val closed: List<Pair<String, Double>>) : Jsonable
 
 @AutoDeployable
-class ReportVerticle: AbstractKotoedVerticle() {
+class ReportVerticle : AbstractKotoedVerticle() {
 
     private val template = "results\\.json".toRegex()
     private val successfulStatus = "SUCCESSFUL"
 
-    private val hardness = compareByDescending<String> {
-        when(it) {
-            "Impossible" -> 5
-            "Hard" -> 4
-            "Normal" -> 3
-            "Easy" -> 2
-            "Trivial" -> 1
-            else -> 0
+    private val String?.grade: Double
+        get() = when (this) {
+            "Impossible" -> 10.0
+            "Hard" -> 7.0
+            "Normal" -> 4.0
+            "Easy" -> 1.0
+            "Trivial" -> 0.0
+            else -> 0.0
         }
-    }
 
-    private fun calcScore(sr: SubmissionResultRecord): Int {
-        if(template !in sr.type) return 0
+    private val List<Double>.grade: Double
+        get() = when (size) {
+            0 -> 0.0
+            1 -> this[0]
+            else -> {
+                val (first, second) = this
+                if (first == second) first + 1.0 else first
+            }
+        }
+
+    private fun calcScore(sr: SubmissionResultRecord): Double {
+        if (template !in sr.type) return 0.0
 
         val content: KotoedRunnerTestRun = (sr.body as JsonObject).snakeKeys().toJsonable()
 
-        val lessonData =
+        val goodLessonData =
                 content.data
-                .groupBy { it.packageName.split('.').first() }
-                .mapValues { (_, results) ->
-                    results.groupBy { it.methodName }
-                            .filter { (_, v) -> v.flatMap { it.results }.all { it.status == successfulStatus } }
-                            .map { (_, v) -> v.flatMap { it.tags }.firstOrNull().orEmpty() }
-                            .sortedWith(hardness)
+                        .groupBy { it.packageName.split('.').first() }
+                        .mapValues { (_, results) ->
+                            results.groupBy { it.methodName }
+                                    .filter { (_, v) -> v.flatMap { it.results }.all { it.status == successfulStatus } }
+                                    .map { (_, v) -> v.flatMap { it.tags }.firstOrNull().grade }
+                                    .sortedDescending()
+                        }
+
+        val fullLessonData =
+                content.data
+                        .groupBy { it.packageName.split('.').first() }
+                        .mapValues { (_, results) ->
+                            results.groupBy { it.methodName }
+                                    .map { (_, v) -> v.flatMap { it.tags }.firstOrNull().grade }
+                                    .sortedDescending()
+                        }
+
+        val lessonData: Map<String, Pair<List<Double>, List<Double>>> =
+                fullLessonData.entries.fold(mutableMapOf()) { acc, (id, fullData) ->
+                    acc.also {
+                        it[id] = (goodLessonData[id] ?: emptyList()) to fullData
+                    }
                 }
 
-        var total = 0
+        var total = 0.0
 
-        for((_, data) in lessonData) {
-
-            var lessonTotal = 0
-            val (first, second, third) = data + listOf("", "", "")
-
-            when (first) {
-                "Impossible" -> lessonTotal += 8
-                "Hard" -> lessonTotal += 5
-                "Normal" -> lessonTotal += 2
-                "Easy", "Trivial" -> lessonTotal += 1
-            }
-
-            when (second) {
-                "Impossible" -> lessonTotal += 3
-                "Hard" -> lessonTotal += 2
-                "Normal", "Easy" -> lessonTotal += 1
-            }
-
-            when (third) {
-                "Impossible", "Hard", "Normal" -> lessonTotal += 1
-            }
-
-            total += lessonTotal
+        for ((_, data) in lessonData) {
+            val (goodData, fullData) = data
+            val score = if (0.0 == fullData.grade) 0.0 else goodData.grade / fullData.grade
+            total += score
         }
 
         return total
     }
 
 
-    suspend fun makeReport(request: ReportRequest, subStates: List<String>): List<Pair<String, Int>> {
+    suspend fun makeReport(request: ReportRequest, subStates: List<String>): List<Pair<String, Double>> {
         val date = request.date ?: OffsetDateTime.now()
 
         val q = ComplexDatabaseQuery(Tables.SUBMISSION_RESULT)
@@ -86,7 +92,7 @@ class ReportVerticle: AbstractKotoedVerticle() {
                                         ComplexDatabaseQuery(Tables.PROJECT)
                                                 .join("denizen", "denizen_id")
                                 )
-                ).filter(subStates.map{ "submission.state == \"$it\"" }.joinToString(" or "))
+                ).filter(subStates.map { "submission.state == \"$it\"" }.joinToString(" or "))
 
         val resp = dbQueryAsync(q)
 
@@ -99,7 +105,9 @@ class ReportVerticle: AbstractKotoedVerticle() {
                         it.toRecord<SubmissionResultRecord>()
                     }
                 }
-                .map { (k, v) -> k.orEmpty() to (v.map(this::calcScore).max() ?: 0) }
+                .map { (k, v) ->
+                    k.orEmpty() to (v.map(this::calcScore).max() ?: 0.0)
+                }
                 .sortedBy { it.first }
     }
 
