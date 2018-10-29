4a1c7635245c6e31869e5dcc3721b69c71bd9de1
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2018-10-29 19:53:48 +0300

    Some more scoring tags

---
+++ kotoed-server/src/main/resources/db/migration/V73__More_scoring_tags.sql
@@ -0,0 +1,3 @@
+INSERT INTO tag (name, style) values ('0.8',  '{"backgroundColor": "white", "color": "#ee0000"}');
+INSERT INTO tag (name, style) values ('0.7',  '{"backgroundColor": "white", "color": "#dd0000"}');
+INSERT INTO tag (name, style) values ('0.6',  '{"backgroundColor": "white", "color": "#cd0000"}');
