bd96ce4dcca0e0b14984c19538daf0613c50f870
Kirill Gagarski <gagarski@kspt.icc.spbstu.ru>
2019-11-05 20:20:20 +0300

    Even more scoring tags

---
+++ kotoed-server/src/main/resources/db/migration/V79__Even_more_scoring_tags.sql
@@ -0,0 +1,12 @@
+-- multiplicative
+INSERT INTO tag (name, style) values ('0.95',  '{"backgroundColor": "white", "color": "#ff0000"}'); -- ¯\_(ツ)_/¯ cannot be more red than red
+INSERT INTO tag (name, style) values ('0.85',  '{"backgroundColor": "white", "color": "#f60000"}');
+INSERT INTO tag (name, style) values ('0.75',  '{"backgroundColor": "white", "color": "#e60000"}');
+
+-- additive
+INSERT INTO tag (name, style) values ('-0.03',  '{"backgroundColor": "white", "color": "#e30000"}');
+INSERT INTO tag (name, style) values ('-0.04',  '{"backgroundColor": "white", "color": "#e90000"}');
+INSERT INTO tag (name, style) values ('-0.06',  '{"backgroundColor": "white", "color": "#cf0000"}');
+INSERT INTO tag (name, style) values ('-0.07',  '{"backgroundColor": "white", "color": "#d30000"}');
+INSERT INTO tag (name, style) values ('-0.08',  '{"backgroundColor": "white", "color": "#d60000"}');
+INSERT INTO tag (name, style) values ('-0.09',  '{"backgroundColor": "white", "color": "#da0000"}');
