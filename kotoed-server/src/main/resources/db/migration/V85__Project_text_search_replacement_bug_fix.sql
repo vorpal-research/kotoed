b335497edae993d2c6ca57120c93f109ea1719d4
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2021-09-17 14:18:22 +0300

    Fix in previous migration

---
+++ kotoed-server/src/main/resources/db/migration/V85__Project_text_search_replacement_bug_fix.sql
@@ -0,0 +1,27 @@
+delete from project_text_search_supplement where true;
+
+insert into project_text_search_supplement(project_id, document)
+select id, document
+from project_text_search_backing;
+
+create or replace function pts_make_trigger_submission_tag() returns trigger as
+$$
+declare
+    proj_id integer;
+    doc tsvector;
+begin
+    select project_id into proj_id from submission s where old.submission_id = id;
+    select document into doc from project_text_search_backing where id = proj_id;
+    insert into project_text_search_supplement (project_id, document)
+    values (proj_id, doc)
+    on conflict(project_id) do update set document = doc;
+    return NULL;
+end;
+$$ LANGUAGE plpgsql;;
+
+drop trigger if exists pts_trigger_submission_tag on submission_tag;
+create trigger pts_trigger_submission_tag
+    after insert or update or delete
+    on submission_tag
+    for each row
+execute function pts_make_trigger_submission_tag();
