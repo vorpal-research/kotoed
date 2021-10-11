af3d78a0a7479afaf98568500d015a0583667e92
Mikhail Belyaev <belyaev@kspt.icc.spbstu.ru>
2021-10-11 18:13:31 +0300

    Trying to fix comment translation bug

--- kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/util/code/PsiUtil.kt
+++ kotoed-server/src/main/kotlin/org/jetbrains/research/kotoed/util/code/PsiUtil.kt
@@ -2,6 +2,10 @@ package org.jetbrains.research.kotoed.util.code
 
 import com.intellij.openapi.util.Disposer
 import com.intellij.psi.PsiElement
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSeverity
+import org.jetbrains.kotlin.cli.common.messages.CompilerMessageSourceLocation
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
 import org.jetbrains.kotlin.cli.jvm.compiler.EnvironmentConfigFiles
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreEnvironment
 import org.jetbrains.kotlin.config.CompilerConfiguration
@@ -14,6 +18,7 @@ import org.jetbrains.kotlin.psi.psiUtil.endOffset
 import org.jetbrains.kotlin.psi.psiUtil.startOffset
 import org.jetbrains.research.kotoed.code.Filename
 import org.jetbrains.research.kotoed.code.Location
+import org.jetbrains.research.kotoed.util.Loggable
 import org.jetbrains.research.kotoed.util.contains
 
 val KtFile.namedFunctions get() = collectDescendantsOfType<KtNamedFunction>()
@@ -57,11 +62,24 @@ fun KtFile.blockByLocation(loc: Location): PsiElement {
     return this
 }
 
-inline fun <R> temporaryEnv(body: (KotlinCoreEnvironment) -> R): R {
+inline fun <R> Loggable.temporaryEnv(body: (KotlinCoreEnvironment) -> R): R {
     val disposable = Disposer.newDisposable()
     val env = KotlinCoreEnvironment.createForProduction(
             disposable,
-            CompilerConfiguration(),
+            CompilerConfiguration().apply {
+                  put(CLIConfigurationKeys.MESSAGE_COLLECTOR_KEY, object : MessageCollector {
+                      override fun clear() {}
+                      override fun hasErrors(): Boolean = false
+                      override fun report(
+                          severity: CompilerMessageSeverity,
+                          message: String,
+                          location: CompilerMessageSourceLocation?
+                      ) {
+                          log.info("Kotlin compiler message: [${severity.presentableName}]: $message")
+                      }
+
+                  })
+            },
             EnvironmentConfigFiles.JVM_CONFIG_FILES
     )
     try {
